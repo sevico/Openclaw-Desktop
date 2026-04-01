cf051d8c0de1b1a0f5139aab21b351feaf372c74
sevico <swkhack@gmail.com>
2026-04-01 15:45:56 +0800

    fix: 在所有平台(windows,linux)隐藏默认菜单栏

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu } from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
@@ -71,6 +71,7 @@ function setupAppIcon() {
 
 // 创建主窗口
 function createWindow() {
+  Menu.setApplicationMenu(null);
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
